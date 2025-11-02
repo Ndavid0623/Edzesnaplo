75895ea5bf8e9f5da56e700e814046230e2af62e
Davidomka23 <124671777+Davidomka@users.noreply.github.com>
2025-11-02 19:08:51 +0100

    Migrate

--- Edzesnaplo/database/migrations/2025_11_02_175924_create_edzes_table.php
+++ Edzesnaplo/database/migrations/2025_11_02_175924_create_edzes_table.php
@@ -4,18 +4,22 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
     public function up(): void
     {
-        Schema::create('edzes', function (Blueprint $table) {
+        Schema::create('workouts', function (Blueprint $table) {
             $table->id();
-            $table->timestamps();
+            $table->date('date');
+            $table->string('type');
+            $table->string('exercise');
+            $table->integer('duration');
+            $table->text('notes')->nullable();
         });
     }
+
 
     /**
      * Reverse the migrations.
