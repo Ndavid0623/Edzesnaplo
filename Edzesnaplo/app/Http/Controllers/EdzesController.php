7c857f2b8299447cd00c9a2ebed724ea56ca5aaf
Davidomka23 <124671777+Davidomka@users.noreply.github.com>
2025-11-02 19:04:19 +0100

    Alapok legyártása

---
+++ Edzesnaplo/app/Http/Controllers/EdzesController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Edzes;
+use Illuminate\Http\Request;
+
+class EdzesController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Edzes $edzes)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Edzes $edzes)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Edzes $edzes)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Edzes $edzes)
+    {
+        //
+    }
+}
